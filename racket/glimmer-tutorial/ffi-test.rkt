35ec2d1edb19b8f9abf1d8abed8dda77295d9e30
Paul Ojanen <paul@ojanen.com>
2015-02-02 12:13:07 -0500

    removed old attempts

--- racket/glimmer-tutorial/ffi-test.rkt
+++
@@ -1,9 +0,0 @@
-#lang racket/base
-(require ffi/unsafe
-         ffi/unsafe/define)
-
-(define the-lib (ffi-lib "mylib1"))
-
-(define-ffi-definer define-mylib the-lib)
-
-(define-mylib isquare (_fun _int -> _int))

138d9d745b4714c9c2bc14812f30317620a86eec
Paul Ojanen <paul@ojanen.com>
2015-02-02 13:00:21 -0500

    cleaned up code

--- haskell3/ffi-test.rkt
+++ haskell3/ffi-test.rkt
@@ -2,23 +2,24 @@
 (require ffi/unsafe
          ffi/unsafe/define)
 
-(printf "Loading .so file...\n")
+(printf "Loading .so files...\n")
 
-(define the-lib (ffi-lib "./libexample"))
-(define-ffi-definer define-mylib the-lib)
+(define lib-example (ffi-lib "./libexample"))
+(define-ffi-definer define-example lib-example)
 
-(define rts-lib (ffi-lib "./libinit"))
-(define-ffi-definer define-rts rts-lib)
+(define lib-rts (ffi-lib "./librts"))
+(define-ffi-definer define-rts lib-rts)
 
 (printf "Shared objects loaded without error!\n")
-
-(define-mylib entrypoint (_fun _int -> _int))
-(printf "Aquired entrypoint function\n")
 
 (define-rts init (_fun -> _int))
 (printf "Aquired init function\n")
 
 (init)
+(printf "Initialized RTS\n")
+
+(define-example entrypoint (_fun _int -> _int))
+(printf "Aquired entrypoint function\n")
 
 (printf "Now calling entrypoint:\n")
 (entrypoint 33)

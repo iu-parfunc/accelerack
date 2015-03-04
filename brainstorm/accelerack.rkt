131587e41dcb41f6f53db0de6769dd9daab0df5b
Paul Ojanen <engineer@alum.mit.edu>
2015-03-04 11:44:11 -0500

    n-ary fn defn

--- brainstorm/accelerack.rkt
+++ brainstorm/accelerack.rkt
@@ -7,19 +7,26 @@
   )
 
 (define-syntax (acc stx)
-  (syntax-case stx (define view load run)
-   
+  (syntax-case stx (define load run)
+    
+    ;Defines allow redefinition!
+    
     ; Variable definition
-    [(acc (define x exp)) (begin (void)#|add_to_hashtable|#
+    [(acc (define x exp)) (identifier? #'x)
+                          (begin (hash-set! ht (syntax->datum (syntax x)) (syntax->datum (syntax exp)))
                                  #'(define x exp))]
     
     ; Function definition
-    [(acc (define (fn x) body)) (begin
-                                  (hash-set! ht (syntax->datum (syntax fn)) (syntax->datum (syntax body)))
-                                  #'(define (fn x) body))]  ;<--separate handling for fn defn...todo
+    [(acc (define (fn x ...) body)) (begin
+                                  (hash-set! ht (syntax->datum (syntax fn)) (syntax->datum (syntax (λ (x ...) body))))
+                                  #'(define (fn x ...) body))]
    
+    ; Generic use of higher order function, not well-understood in here yet
+    ;[(acc (f (fn x) body)) (begin (hash-set! ht (syntax->datum (syntax fn)) (syntax->datum (syntax body)))
+    ;                                 #'(f (fn x) body))]  ;<--separate handling for fn defn...todo
+            
     ; Designed to be called in Definitions Window to create a run-time binding to the AccRack hashtable
-    [(acc view) (datum->syntax #'acc ht)]
+    [(acc) (datum->syntax #'acc ht)]
     
     ; Designed to be called in Interactions Windows to reset the REPL compilation environment's hashtable
     [(acc load ht2) (begin (set! ht (syntax->datum (syntax ht2))) #'(display ht2))]
@@ -34,11 +41,14 @@
     [(acc exp exp2 x ...) #'(begin (acc exp) (acc exp2 x ...))]
     ))
 
-(acc 
+(acc
+ (define y 25)
  (define (sqr x) (* x x))
  )
 
-(define ht (acc view)) ; creates run-time binding of hashtable
+(define ht (acc)) ; creates run-time binding of hashtable
+
+ht
 
 ;(acc load ht) ; populates REPL's compilation environment with the hashtable
 ;; This could be more automated when this is packaged into one or more modules:

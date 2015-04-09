7316bb294477189583c02fdc1cd83112aaacb360
Paul Ojanen <engineer@alum.mit.edu>
2015-04-09 11:50:00 -0400

    move working copy of trial.rkt

--- example_apps/trial.rkt
+++ example_apps/trial.rkt
@@ -1,45 +1,47 @@
 #lang htdp/isl+
 
-;; a Base* is one of:
-;; - 'Int
-;; - 'Float
+;; Front-most facing API
 
-;; a Shape is a [ListOf Nat]
-;; an Index is a Shape
+(require "../lib/accelerack.rkt")
 
-;; a NonEmptyShape is a Shape containing no zeroes
-;; empty-shape? : Shape -> Boolean
-(define (empty-shape? sh)
-  (andmap positive? sh))
+; Empty arrays
+(generate '(0) (const 4))
+(generate '(3 4 0 1) (const 4))
 
-;; gen-indices : NonEmptyShape -> [ListOf Index]
-;; (gen-indices sh) produces a complete list of all valid indices in sh
-;; Confirmed linear run time
-(define (gen-indices sh)
-  (if
-   (empty? sh) (list empty)
-   (local [(define sub-indices (gen-indices (rest sh)))]
-     (foldr append empty
-            (build-list (first sh)
-                        (λ (n) (map (λ (index) (cons n index))
-                                    sub-indices)))))))
-  
-;; an Element* is a [ListOf Base*]
-;; an Element is a [ListOf Number]
+; Singleton non-arrays
+(generate '() (const 4))
+(generate '() (const (list 5 6)))
 
-;; a Payload stores all the elements of the array
-;; currently represented as a [... [ListOf [ListOf Element]]...]
+; Array of length one
+(generate '(1) (const (list 5 6)))
 
-;; an Array is a (make-array Shape Element* Payload)
-(define-struct array [shape element-type payload])
+; Note: The two above examples differ only in dimensionality,
+;       and so they are indexed differently, (Z) vs (Z 0).
+; This is consistent with Accelerack's internal representation and Accelerate itself.
 
-;; generate : Shape [Index -> Element] -> Array
-;; (generate sh fn) produces an array of shape sh
-;; with elements defined by fn applied to their respective index
-;(define (generate sh fn)
-;  ((gen-indices sh)
+; Singleton payloads:  two ways of returning singleton payload elements
+(generate '(5) sqr)  ;as numbers
+(generate '(5) list) ;as single-element lists
+
+; Multi-dimensional array:  function arity must match number of dimensions
+(generate '(5 5) +)
+(generate '(2 4 4) *)
+
+; Tuple payloads:  lists for tuples
+(generate '(2 3) (λ (i j) (list i j)))
+(generate '(10) (λ (i) (list i (sqr i))))
+
+; Teaching languages do not allow var-arg lambdas
+; Full Racket will allow this example
+(require racket)
+(generate '(2 1 1 1 2) (λ ls (length ls)))
+
+;--------------------------------------------------------------------------------------
+; Below is old
+;--------------------------------------------------------------------------------------
 
 ;; dot-product : [ListOf Number] [ListOf Number] -> Number
 ;; (dot-product p1 p2) produces p1·p2, the algebraic dot product
 ;(define (dot-product p1 p2)
 ;  (foldr
+

1b31593ccf6466b8f555fd5ae79199e0a202d3b4
Paul Ojanen <engineer@alum.mit.edu>
2015-04-15 14:55:56 -0400

    doc Mandelbrot

--- example_apps/mandelbrot.rkt
+++ example_apps/mandelbrot.rkt
@@ -5,19 +5,26 @@
 (require "../lib/types.rkt")
 
 (require 2htdp/image)
-(define D 10)
 
-(define W 10)
-(define H 10)
+; Size in pixels of each cell
+(define D 2)
 
-(define I 10)
+;; Number of cells
+(define W 30)
+(define H 30)
 
-(define X_MAX 2.0)
+;; Max Iterations
+(define I 12)
+
+;; Boundaries of computation
+(define X_MAX 1.0)
 (define X_MIN -2.0)
-(define Y_MAX 2.0)
-(define Y_MIN -2.0)
+(define Y_MAX 1.0)
+(define Y_MIN -1.0)
 
-(define (interp a amax amin bmax bmin)
+;; interp : Number Number Number Number Number -> Number
+;; Interpolates b in b-range from a in a-range
+(define (interp a amin amax bmin bmax)
   (+ bmin (* (/ (- a amin) (- amax amin))
              (- bmax bmin))))
 
@@ -45,21 +52,27 @@
       (let ([x2 (+ (- (* x x) (* y y)) x0)]
             [y2 (+ (* 2.0 x y) y0)])
         (list x0 y0 x2 y2 (add1 i)))))
+
+;; Iterating map
+;; applies update n times to array
 (define (amap/n update array n)
   (cond
     [(zero? n) array]
     [else (amap/n update (amap update array) (sub1 n))]))
 
+;; Computation
 (define result (amap/n update init-array I))
 
+;; Throw out coordinates, keeping only iteration counts
 (define (get-iters array)
   (amap (λ (x0 y0 x y i) i) array 1))
 
-(define (array->palette array)
-  (let ([array-row->palette-row (λ (row) (map first row))])
-    (map array-row->palette-row array)))
+;; Remove one layer
+(define (array->bmp array)
+  (let ([array-row->bmp-row (λ (row) (map first row))])
+    (map array-row->bmp-row array)))
 
-(define bmp (array->palette (get-iters result)))
+(define bmp (array->bmp (get-iters result)))
 
 ;; a Palette is a [Nat -> Color]
 (define colors '(black purple red orange yellow green turquoise blue))
@@ -67,11 +80,12 @@
 (define palette (λ (n)
                   (list-ref colors (floor (interp n I 0 0 (sub1 N))))))
                                  
-
+;; Rendering routine
 (define (draw bmp palette)
   (let ([draw-row (λ (row) (foldr (λ (n bg) (beside (square D 'solid (palette n)) bg))
                                   empty-image
                                   row))])
     (foldr (λ (row bg) (above (draw-row row) bg)) empty-image bmp)))
 
+;; Final image
 (draw bmp palette)

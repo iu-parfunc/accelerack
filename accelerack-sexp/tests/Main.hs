b5e23636e9cbd53eaa524877f4d1eb3211554b24
Kyle Carter <kylcarte@gmail.com>
2015-10-02 11:40:58 -0400

    Added tests

--- accelerack-sexp/tests/Main.hs
+++ accelerack-sexp/tests/Main.hs
@@ -1,7 +1,26 @@
+{-# LANGUAGE LambdaCase #-}
 
+module Main where
 
+import Language.Haskell.Interpreter
+import Accelerack.Parse
+import Accelerack.Gen
+import Accelerack.Run
 
+main :: IO ()
+main = do
+  interp $ do
+    setImportsQ [("Data.List",Just "L")]
+    interpret "L.map (+1) [1..4]" (as :: [Int])
+  interp $ do
+    setImportsQ [("Data.List",Just "L")]
+    eval "map (+2) [3..8]"
 
-main = putStrLn "Hello world"
-
+interp :: Show a => Interpreter a -> IO ()
+interp m = do
+  res <- runInterpreter m
+  putStrLn $ replicate 20 '-'
+  case res of
+    Left err -> putStrLn $ "Interpreter Error: " ++ show err
+    Right a  -> print a
 

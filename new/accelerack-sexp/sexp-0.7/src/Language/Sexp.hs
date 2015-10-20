df57f8073628d98fca66406cf94c770504068610
Kyle Carter <kcarter@galois.com>
2015-10-20 17:22:28 -0400

    Moved accelerack-sexp into new repo organization

--- new/accelerack-sexp/sexp-0.7/src/Language/Sexp.hs
+++
@@ -1,9 +0,0 @@
-module Language.Sexp (
-        module Data.Sexp,
-        module Language.Sexp.Parser,
-        module Language.Sexp.Printer
-    ) where
-
-import Data.Sexp
-import Language.Sexp.Parser
-import Language.Sexp.Printer

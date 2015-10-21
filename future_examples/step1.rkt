96bd0704c22d9bf7393aecca7a9a07ce8312f0ba
Ryan Newton <rrnewton@gmail.com>
2015-10-21 13:41:14 -0400

    Minor tweaks to scifi examples

--- future_examples/step1.rkt
+++ future_examples/step1.rkt
@@ -2,8 +2,10 @@
 
 (require accelerack)
 ; (require "accelerack.rkt") ;; Dummy exports to run this.
+(require rackunit)
 
-;; Debatable 
+;; Debatable whether this should be called "array" or "acc-array"
+;; It may be a good idiom to import the module with prefix "acc-"
 (array 99)        ;; Zero dim array.
 (array (1 2 3 4)) ;; One dim array
 (array #(3 #f))   ;; Zero dim array of tuple.
@@ -11,11 +13,10 @@
 (define x (array ((2 3)
                   (4 5)))) ;; Two dim array
 
-(array? x)
+(check-true (array? x))
 
 ;; Run Racket-side array computation:
 (map add1 x)
 
 ;; But we don't lose map's normal functionality on lists:
 (map add1 '(9 10 11))
-
--- future_examples/step2.rkt
+++ future_examples/step2.rkt
@@ -16,13 +16,26 @@
 
 ;; This should still be true, whether or not the computation
 ;; has happened eagerly or lazily:
-(array? vec2)
+(check-true (array? vec2))
 
 ;; This runs the computation on the GPU, and returns a Racket-side array:
-(array? (acc vec2))
+(check-true (array? (acc vec2)))
 
 ;; Whether or not it was forced before, this forces the computation on the racket side.
 ;; With lazy semantics we would see any exceptions here:
 (array->list vec2)
 ;; One reason to use lazy semantics is so we don't compute things on the Racket side
 ;; that should really only be done on the GPU side.
+
+;; Accelerate *does* have array level conditionals, so the following could be valid: 
+(define-acc vec3 (if (= (! vec2 1) 3) vec vec2))
+
+;; The fact that (array? vec3), as opposed to (procedure? vec3) is due
+;; to type inference in Accelerack.
+
+;; But what about this?:
+; (define-acc fn (if (= (! vec2 1) 3) sqr sqr))
+;; (1) That could be a type error in the front-end.  The typing rule for conditionals
+;;     should rule out arrow types in its branches.
+;; (2) It could also *work*, if we have commuting conversions in our normalization pass.
+

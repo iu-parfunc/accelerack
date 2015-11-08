20ca2d4e2e4bbed16e19747e36b59228f0e1f91e
paven <paventh.v@gmail.com>
2015-11-08 08:28:53 -0500

    acc-map completed and contracts are added for acc_allocate

--- acc_rkt/accelerack/main.rkt
+++ acc_rkt/accelerack/main.rkt
@@ -4,10 +4,12 @@
            accelerack/src/acc_arrayutils
            accelerack/src/acc_header
            accelerack/src/acc_syntax
-	   accelerack/src/acc_parse)
+	   accelerack/src/acc_parse
+           accelerack/src/acc_racket_ops)
 
 (#%provide (all-from accelerack/src/acc_allocate)
            (all-from accelerack/src/acc_arrayutils)
            (all-from accelerack/src/acc_header)
            (all-from accelerack/src/acc_syntax)
-	   (all-from accelerack/src/acc_parse))
+	   (all-from accelerack/src/acc_parse)
+           (all-from accelerack/src/acc_racket_ops))

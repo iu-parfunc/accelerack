2c4df029bbb2852586c827f7d95b754336c5b36c
paven <paventh.v@gmail.com>
2015-11-10 09:12:32 -0500

    Debugging travis

--- acc_hs/.prevalidate.sh
+++ acc_hs/.prevalidate.sh
@@ -5,5 +5,6 @@ set -xe
 stack setup --no-terminal 
 stack test --only-snapshot --no-terminal 
 cabal install accelerate --enable-shared
+cabal install accelerate-io --enable-shared
 make clean
 make

2118a3f19d792a320c2945feb0ba6f8e37ee7149
Sam Tobin-Hochstadt <samth@cs.indiana.edu>
2015-11-23 10:26:23 -0500

    Fix dependencies

--- acc_rkt/info.rkt
+++ acc_rkt/info.rkt
@@ -3,3 +3,5 @@
 ;; omit the following paths from testing during 'raco test'
  
 (define test-omit-paths (list "info.rkt" "accelerack/main.rkt"))
+(define deps '("c-defs"
+               ("base" #:version "6.2.1")))

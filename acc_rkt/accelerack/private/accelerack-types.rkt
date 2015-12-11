97d55af552e3e7b54de9999aef65feb03b716135
Ryan Newton <rrnewton@gmail.com>
2015-12-11 03:42:20 -0500

    Start to add more types and extend the grammar a bit

--- acc_rkt/accelerack/private/accelerack-types.rkt
+++ acc_rkt/accelerack/private/accelerack-types.rkt
@@ -5,10 +5,16 @@
 (provide
  ;; Types:
  Bool Int Double Array
+ Num SExp
 
  ;; Syntax for type ascription:
  :
+
+ ;; Reexports:
+ ->
  )
+
+(require (only-in racket/contract ->))
 
 (define-syntax (Bool stx)
   (raise-syntax-error 'error "Bool type used outside of Accelerate block" stx))
@@ -18,7 +24,15 @@
   (raise-syntax-error 'error "Double type used outside of Accelerate block" stx))
 (define-syntax (Array stx)
   (raise-syntax-error 'error "Array type constructor used outside of Accelerate block" stx))
+
 ;(define-syntax (-> stx)
 ;  (raise-syntax-error 'error "function (->) type constructor used outside of Accelerate block" stx))
+
 (define-syntax (: stx)
   (raise-syntax-error 'error "colon (:) syntax for type annotation should be used in an Accelerate block" stx))
+
+(define-syntax (Num stx)
+  (raise-syntax-error 'error "Num type constructor used outside of Accelerate block" stx))
+
+(define-syntax (SExp stx)
+  (raise-syntax-error 'error "SExp type constructor used outside of Accelerate block" stx))

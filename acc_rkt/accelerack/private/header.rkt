d1ee353687736a43d40b50ec4f070a8a70f200dd
Ryan Newton <rrnewton@gmail.com>
2015-12-11 03:00:59 -0500

    Disable the reading of acc_types.h at expansion time, which Sam found made DrRacket unhappy.  Add explanation of same.

--- acc_rkt/accelerack/private/header.rkt
+++ acc_rkt/accelerack/private/header.rkt
@@ -7,28 +7,6 @@
          racket/runtime-path
          c-defs)
 
-;; Import the acc type definitions from the c header file to maintain consistent naming with haskell
-(define-runtime-path acc_types "../../../acc_c/acc_types.h")
-(define ACC_TYPES (c-defs (path->string acc_types)))
-(define-values (C_INT
-                C_DOUBLE
-                C_BOOL
-                ACC_PAYLOAD_PTR
-                SCALAR_PAYLOAD
-                TUPLE_PAYLOAD
-                ACC_ARRAY_PTR
-                C_PTR
-                RKT_PAYLOAD_PTR)
-  (ACC_TYPES "%d" "C_INT"
-                  "C_DOUBLE"
-                  "C_BOOL"
-                  "ACC_PAYLOAD_PTR"
-                  "SCALAR_PAYLOAD"
-                  "TUPLE_PAYLOAD"
-                  "ACC_ARRAY_PTR"
-                  "C_PTR"
-                  "RKT_PAYLOAD_PTR"))
-
 (provide
 
    ;; The public type for Accelerate/Accelerack arrays:
@@ -65,17 +43,60 @@
    [shape _segment-pointer]
    [data _segment-pointer]))
 
+
+;; Import the acc type definitions from the c header file to maintain consistent naming with haskell
+#;
+(begin
+  (define-runtime-path acc_types "../../../acc_c/acc_types.h")
+  (define ACC_TYPES (c-defs (path->string acc_types)))
+  (define-values (C_INT
+                  C_DOUBLE
+                  C_BOOL
+                  ACC_PAYLOAD_PTR
+                  SCALAR_PAYLOAD
+                  TUPLE_PAYLOAD
+                  ACC_ARRAY_PTR
+                  C_PTR
+                  RKT_PAYLOAD_PTR)
+    (ACC_TYPES "%d" "C_INT"
+               "C_DOUBLE"
+               "C_BOOL"
+               "ACC_PAYLOAD_PTR"
+               "SCALAR_PAYLOAD"
+                  "TUPLE_PAYLOAD"
+                  "ACC_ARRAY_PTR"
+                  "C_PTR"
+                  "RKT_PAYLOAD_PTR")))
+
+;; WARNING: DELICATE INVARIANT.
+;; ----------------------------------------
+;; Currently [2015.12.11] the below cases of scalar have
+;; to exactly match the order of the defines in acc_types.h.
+;;
+;; Invoking the C compiler at expansion time is a heavy-weight
+;; solution and it makes the DrRacket background syntax checker
+;; unhappy, which is a problem since LOTS of modules depend on this
+;; one.
+;;
+;; In the future, it would be good to have a SINGLE POINT OF ORIGIN
+;; for this information, but that should probably be something other
+;; than a C header file.  For instance, it could just be an SExp or
+;; text file that everybody reads.  Or the Racket or Haskell code
+;; could generate this information for the other.
+;; ----------------------------------------
+
 ;; Enum for storing the type of scalar
 (define scalar
-  (_enum `(c-int = ,C_INT
-           c-double = ,C_DOUBLE
-           c-bool = ,C_BOOL
-           acc-payload-ptr = ,ACC_PAYLOAD_PTR ;; segment-ptr
-           scalar-payload = ,SCALAR_PAYLOAD
-           tuple-payload = ,TUPLE_PAYLOAD
-           acc-manifest-array-ptr = ,ACC_ARRAY_PTR
-           c-ptr = ,C_PTR
-           rkt-payload-ptr = ,RKT_PAYLOAD_PTR)))
+  (_enum `(c-int           ; = ,C_INT
+           c-double        ; = ,C_DOUBLE
+           c-bool          ; = ,C_BOOL
+           acc-payload-ptr ; = ,ACC_PAYLOAD_PTR ;; segment-ptr
+           scalar-payload  ; = ,SCALAR_PAYLOAD
+           tuple-payload   ; = ,TUPLE_PAYLOAD
+           acc-manifest-array-ptr ; = ,ACC_ARRAY_PTR
+           c-ptr           ; = ,C_PTR
+           rkt-payload-ptr ; = ,RKT_PAYLOAD_PTR
+           )))
 
 ;; Number of scalar enum values defined
 (define scalar-length 7)

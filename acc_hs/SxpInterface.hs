9b2aeb5d41afd52dcade18f0475bfcf2f06455b9
Kyle Carter <kcarter@galois.com>
2015-12-21 19:46:37 -0500

    added sexp parser to SxpInterface, more refactoring

--- acc_hs/SxpInterface.hs
+++ acc_hs/SxpInterface.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE LambdaCase #-}
 {-# Language TypeOperators #-}
 {-# LANGUAGE ForeignFunctionInterface #-}
 {-# LANGUAGE CPP                      #-}
@@ -52,10 +54,10 @@ entrypoint x = do
   print "Hello from Haskell"
   return x
 
-getReferenceStr :: Int -> Ptr AccArray -> IO String
-getReferenceStr refNum arr = do
+getRef :: Ptr AccArray -> Int -> IO String
+getRef arrs refNum = do
   let ptrSize = 8 :: Int
-  AccArray atyp ashp adata <- peek arr
+  AccArray atyp ashp adata <- peek arrs
   Segment s1 t1 dsg <- peek adata
   adata2 <- peekByteOff dsg (refNum * ptrSize)
   AccArray atyp' ashp' adata3 <- peek adata2
@@ -68,91 +70,74 @@ getReferenceStr refNum arr = do
   let arrayStrFn = case Prelude.length sh1 of
         0 -> let psh = (Proxy :: Proxy DIM0)
           in case s1ts' of
-            0 -> arrayReferenceStr psh (Proxy :: Proxy Int32)
-            1 -> arrayReferenceStr psh (Proxy :: Proxy Double)
+            0 -> arrayRef psh (Proxy :: Proxy Int32)
+            1 -> arrayRef psh (Proxy :: Proxy Double)
         1 -> let psh = (Proxy :: Proxy DIM1)
           in case s1ts' of
-            0 -> arrayReferenceStr psh (Proxy :: Proxy Int32)
-            1 -> arrayReferenceStr psh (Proxy :: Proxy Double)
+            0 -> arrayRef psh (Proxy :: Proxy Int32)
+            1 -> arrayRef psh (Proxy :: Proxy Double)
         2 -> let psh = (Proxy :: Proxy DIM2)
           in case s1ts' of
-            0 -> arrayReferenceStr psh (Proxy :: Proxy Int32)
-            1 -> arrayReferenceStr psh (Proxy :: Proxy Double)
+            0 -> arrayRef psh (Proxy :: Proxy Int32)
+            1 -> arrayRef psh (Proxy :: Proxy Double)
         3 -> let psh = (Proxy :: Proxy DIM3)
           in case s1ts' of
-            0 -> arrayReferenceStr psh (Proxy :: Proxy Int32)
-            1 -> arrayReferenceStr psh (Proxy :: Proxy Double)
+            0 -> arrayRef psh (Proxy :: Proxy Int32)
+            1 -> arrayRef psh (Proxy :: Proxy Double)
         4 -> let psh = (Proxy :: Proxy DIM4)
           in case s1ts' of
-            0 -> arrayReferenceStr psh (Proxy :: Proxy Int32)
-            1 -> arrayReferenceStr psh (Proxy :: Proxy Double)
+            0 -> arrayRef psh (Proxy :: Proxy Int32)
+            1 -> arrayRef psh (Proxy :: Proxy Double)
         5 -> let psh = (Proxy :: Proxy DIM5)
           in case s1ts' of
-            0 -> arrayReferenceStr psh (Proxy :: Proxy Int32)
-            1 -> arrayReferenceStr psh (Proxy :: Proxy Double)
+            0 -> arrayRef psh (Proxy :: Proxy Int32)
+            1 -> arrayRef psh (Proxy :: Proxy Double)
         6 -> let psh = (Proxy :: Proxy DIM6)
           in case s1ts' of
-            0 -> arrayReferenceStr psh (Proxy :: Proxy Int32)
-            1 -> arrayReferenceStr psh (Proxy :: Proxy Double)
+            0 -> arrayRef psh (Proxy :: Proxy Int32)
+            1 -> arrayRef psh (Proxy :: Proxy Double)
   arrayStrFn sh1 segdata
 
-arrayReferenceStr :: (Ex.AccelerackShape sh, Ex.AccelerackType a) => Proxy sh -> Proxy a -> [CInt] -> Segment -> IO String
-arrayReferenceStr psh pty shp seg = do
+arrayRef :: (Ex.AccelerackShape sh, Ex.AccelerackType a) => Proxy sh -> Proxy a -> [CInt] -> Segment -> IO String
+arrayRef psh pty shp seg = do
   arr <- Ex.toAccArray psh pty shp seg
   let str = replace "Array" "fromList" $ show arr
   return $ "(use (" ++ str ++ ":: Array " ++ Ex.accShapeAnnot psh ++ " " ++ Ex.accTypeAnnot pty ++ "))"
 
-processReference :: String -> Ptr AccArray -> IO String
-processReference ref arr = case refParts of
-    ["id",x]    -> case readMaybe x of
-      Just i -> getReferenceStr i arr
-      _      -> return ref
-    ["map"]     -> return "Data.Array.Accelerate.map"
-    ["zipwith"] -> return "Data.Array.Accelerate.zipWith"
-    ["fold"]    -> return "Data.Array.Accelerate.fold"
-    _           -> return ref
-  where
-  refParts  = Prelude.filter (not . null) $ splitOneOf "() " ref
-
-appendReference :: [String] -> [String]
-appendReference [] = []
-appendReference (x1:x2:xs) = if (x1 == "(id") then (x1 ++ " " ++ x2) : (appendReference xs) else x1 : (appendReference (x2:xs))
-appendReference _ = error "appendReference"
-
-appendList :: [String] -> String
-appendList [] = ""
-appendList (x:xs) = x ++ " " ++ (appendList xs)
-
 -- | Takes an SExpression in a string, takes payloads, and takes pre-allocated result space.
 runAcc :: CString -> Ptr AccArray -> Ptr AccArray -> IO ()
-runAcc x arr res = do
-  str <- peekCString x
-  let exp = splitWs str
-  -- putStrLn $ "SExp-containing string received by Haskell: " ++ str
-  let exp1 = appendReference exp
-  -- putStrLn (show exp1)
-  let exp2 = (L.map (\x1 -> let x2 = unsafePerformIO $ (processReference x1 arr) :: [Char]
-                            in x2) exp1) :: [[Char]] --(processReference (last exp3) arr)
-  -- putStrLn (show exp2)
-  let exp3 = appendList exp2
-  -- putStrLn (show exp3)
-  x1 <- runInterpreter $ setImports ["Prelude", "Data.Array.Accelerate.Interpreter", "Data.Array.Accelerate"] >> eval ("run $ " ++ exp3)
-  let values = fromRight x1
-  let arr = (last (Ds.split " " values))
-  AccArray atyp ashp adata <- peek res
-  Segment s1ls s1ts s1da <- peek adata
-  a1 <- do
-        case (Prelude.fromIntegral s1ts) of
-          0 -> do
-               let arr' = (read arr) :: [Int]
-               -- putStrLn (show arr')
-               let a3 = L.map Prelude.fromIntegral arr' :: [CInt]
-               pokeArray (castPtr s1da :: Ptr CInt) a3
-          1 -> do
-               let arr' = (read arr) :: [Double]
-               -- putStrLn (show arr')
-               let a3 = L.map Prelude.realToFrac arr' :: [CDouble]
-               pokeArray (castPtr s1da :: Ptr CDouble) a3
-          _ -> putStrLn "Dont Care Case !!!\n"
-  new_str <- newCString values
-  putStr "-"-- (exp3 ++ "\n")
+runAcc cs arrs res = do
+  sxp <- Parse.parseAccSxp knownAccOps cs
+  acc <- insertRefs arrs sxp
+  eInterp <- runInterpreter $ do
+    setImports ["Prelude", "Data.Array.Accelerate.Interpreter", "Data.Array.Accelerate"]
+    eval $ "run $ " ++ acc
+  case eInterp of
+    Left err -> fail $ show err
+    Right values -> do
+      AccArray atyp ashp adata <- peek res
+      Segment s1ls s1ts s1da <- peek adata
+      let pokeSegDataFn = case Prelude.fromIntegral s1ts of
+            0 -> let pty = (Proxy :: Proxy CInt)    in pokeSegmentData pty
+            1 -> let pty = (Proxy :: Proxy CDouble) in pokeSegmentData pty
+      pokeSegDataFn s1da $ last $ words values
+
+pokeSegmentData :: forall ty. (Read ty, Storable ty) => Proxy ty -> Ptr () -> String -> IO ()
+pokeSegmentData _ p str = pokeArray (castPtr p :: Ptr ty) (read str :: [ty])
+
+insertRefs :: Ptr AccArray -> Parse.AccSxp -> IO String
+insertRefs arrs = \case
+  Parse.AccRef i  -> getRef arrs i
+  Parse.AccApp es -> unwords . map wrapParens <$> traverse (insertRefs arrs) es
+  Parse.AccAtom a -> return a
+
+wrapParens :: String -> String
+wrapParens a = concat ["(",a,")"]
+
+knownAccOps :: [(String,String)]
+knownAccOps =
+  [ ( "map"     , "Data.Array.Accelerate.map" )
+  , ( "fold"    , "Data.Array.Accelerate.fold" )
+  , ( "zipwith" , "Data.Array.Accelerate.zipWith" )
+  ]
+

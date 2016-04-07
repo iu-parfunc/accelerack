4c94d289e895ac475ac6ed6184ff2e6a64cfd5af
Ryan Newton <rrnewton@gmail.com>
2016-04-07 12:30:44 -0400

    Move the racket package up to top level.

--- acc_rkt/info.rkt
+++
@@ -1,7 +0,0 @@
-#lang info
-
-;; omit the following paths from testing during 'raco test'
- 
-(define test-omit-paths (list "info.rkt" "accelerack/main.rkt"))
-(define deps '("c-defs"
-               ("base" #:version "6.2.1")))
--- info.rkt
+++ info.rkt
@@ -1,2 +1,7 @@
 #lang info
-(define collection 'multi)
+
+;; omit the following paths from testing during 'raco test'
+ 
+(define test-omit-paths (list "info.rkt" "accelerack/main.rkt"))
+(define deps '("c-defs"
+               ("base" #:version "6.2.1")))

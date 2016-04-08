b9bd80451b6f74beddf1421962b55371b1de3684
Ryan Newton <rrnewton@gmail.com>
2016-04-08 13:43:28 -0400

    update top level info.rkt from other branch

--- info.rkt
+++ info.rkt
@@ -3,9 +3,13 @@
 (define collection 'multi)
 
 (define compile-omit-paths
-  (list "acc_hs"
-        "acc_c"
-        "acc_rkt/tests_disabled"))
+  (list "acc_hs/*"
+        "acc_c/*"
+        "acc_rkt/tests_disabled/*"
+        "info.rkt"))
 
 (define test-omit-paths compile-omit-paths)
 
+(define deps '("c-defs"
+               ; ("base" #:version "6.3")
+               ))

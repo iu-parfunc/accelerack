1c5975bf10f7bb1933372d88509987607fbef9b3
Ryan Newton <rrnewton@gmail.com>
2016-04-13 01:12:19 -0400

    Enable the second image test

--- accelerack/tests/user-facing/images.rkt
+++ accelerack/tests/user-facing/images.rkt
@@ -25,14 +25,13 @@
           (add-bytes (vector-ref c1 2) (vector-ref c2 2))
           (add-bytes (vector-ref c1 3) (vector-ref c2 3))))
 
-;; FINISHME: memory corruption problems atm:
-#;
 (test-case "Convert image->array"
-  (define x (image->acc-array (circle 30 "solid" "red")))
-  (define y (image->acc-array (circle 40 "solid" "green")))
-  (printf "Image 1 shape ~a, image 2 shape ~a\n" (acc-array-shape x) (acc-array-shape y))
-  (define z (zipwith add-cols x y))  
-  (image-width (acc-array->image z))
+  (define x (image->acc-array (circle 30 "solid" (color 100 0 0 255))))
+  (define y (image->acc-array (circle 40 "solid" (color 0 100 0 255))))
+  (printf "  Image 1 shape ~a, image 2 shape ~a\n" (acc-array-shape x) (acc-array-shape y))
+  (define z (zipwith add-cols x y))
+  (define img (acc-array->image z))
+  (printf "  Intersection shape ~a\n" (acc-array-shape z))
+  (printf "  Image shape ~a ~a\n" (image-width img) (image-height img))
+  img
   )
-
-

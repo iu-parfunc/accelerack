770670a39231b7055cae6657c22b1ad88951477e
Ryan Newton <rrnewton@gmail.com>
2016-04-12 23:27:41 -0400

    Add a test for intersection semantics of zipwith

--- accelerack/tests/user-facing/zipwith.rkt
+++ accelerack/tests/user-facing/zipwith.rkt
@@ -6,3 +6,14 @@
   (define y (map (lambda(x) (+ x 1)) x))
   (define z (zipwith * x y))
   (check-equal? (acc-array->sexp z) '(6 6 6 6)))
+
+(test-case "plain-racket: zipwith"
+  (define x (acc-array ((1 2 3)
+                        (4 5 6))))
+  (define y (acc-array ((100 200)
+                        (300 400)
+                        (500 600))))
+  (define z (zipwith + x y))
+  (check-equal? (acc-array->sexp z)
+                '((101 202)
+                  (304 405))))

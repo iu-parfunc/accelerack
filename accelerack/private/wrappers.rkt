4f9fa50c64b2a099feb1d10676315cd38419a818
Mike Vollmer <mike@talkingincircles.net>
2016-04-20 03:13:33 -0400

    Adding 1d stencils

--- accelerack/private/wrappers.rkt
+++ accelerack/private/wrappers.rkt
@@ -52,7 +52,16 @@
                               acc-element? acc-element? acc-element?
                               acc-element?)
                           stencil-boundary? acc-array?
-                          acc-array?)])
+                          acc-array?)]
+          [stencil3    (-> (-> acc-element? acc-element? acc-element?
+                               acc-element?)
+                           stencil-boundary? acc-array?
+                           acc-array?)]
+          [stencil5    (-> (-> acc-element? acc-element? acc-element?
+                               acc-element? acc-element?
+                               acc-element?)
+                           stencil-boundary? acc-array?
+                           acc-array?)])
          generate
          until
          replicate
@@ -95,6 +104,12 @@
 
 (define (stencil5x5 f b x)
   (make-acc-array (acc-stencil5x5 f b (force-acc-array! x))))
+
+(define (stencil3 f b x)
+  (make-acc-array (acc-stencil3 f b (force-acc-array! x))))
+
+(define (stencil5 f b x)
+  (make-acc-array (acc-stencil5 f b (force-acc-array! x))))
 
 (define (generate f . dims)
   (make-acc-array (apply acc-generate f dims)))
